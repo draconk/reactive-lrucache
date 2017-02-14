566be0c69465b99929bbe7ae9a05dd10f8fcb279
formacio <formacio>
2017-02-14 19:55:18 +0100

    asdfsadf

--- src/main/java/com/lepsec/domain/impl/LRUImpl.java
+++ src/main/java/com/lepsec/domain/impl/LRUImpl.java
@@ -3,22 +3,32 @@ package com.lepsec.domain.impl;
 
 import com.lepsec.domain.LRU;
 
+import java.util.LinkedHashMap;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by jnieto on 13/2/17.
  */
 public class LRUImpl implements LRU {
     private static LRUImpl ourInstance = new LRUImpl(3);
+    private LinkedHashMap<Integer, String> linkedHashMap ;
+    private int lruSize;
 
     public static LRUImpl getInstance() {
         return ourInstance;
     }
 
     public LRUImpl(int lruSize) {
-
+        this.linkedHashMap = new LinkedHashMap<>();
+        this.lruSize = lruSize;
     }
 
     public void put(int key, String value) {
-
+        if(this.linkedHashMap.values().size() >= this.lruSize){
+            this.linkedHashMap.remove(this.linkedHashMap.keySet().iterator().next());
+        }
+        this.linkedHashMap.put(key, value);
     }
 
     public String get(int key) throws Exception {
@@ -28,7 +38,12 @@ public class LRUImpl implements LRU {
 
     @Override
     public String toString(){
+        StringBuilder out = new StringBuilder();
 
-        return "";
+        for (String str : this.linkedHashMap.values()){
+            out.append(str);
+            out.append(" ");
+        }
+        return out.toString();
     }
 }
